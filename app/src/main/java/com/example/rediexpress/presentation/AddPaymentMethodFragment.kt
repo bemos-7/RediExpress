ae94aeafe7665a80abaf2a4d99b4ade8215f62be
bemo <bemo.0@mail.ru>
2024-02-14 10:18:04 +0300

    send a package

--- app/src/main/java/com/example/rediexpress/presentation/AddPaymentMethodFragment.kt
+++ app/src/main/java/com/example/rediexpress/presentation/AddPaymentMethodFragment.kt
@@ -7,34 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rediexpress.R
 import com.example.rediexpress.databinding.AddPaymentMethodFragmentBinding
-import com.example.rediexpress.databinding.ProfileFragmentBinding
 
-class ProfileFragment : Fragment() {
+class AddPaymentMethodFragment : Fragment() {
 
-    lateinit var binding: ProfileFragmentBinding
+    lateinit var binding: AddPaymentMethodFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = ProfileFragmentBinding.inflate(inflater)
+        binding = AddPaymentMethodFragmentBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        binding.cardBlankButton.setOnClickListener {
+        binding.backButton.setOnClickListener{
 
-            parentFragmentManager.beginTransaction().replace(R.id.frame_container, AddPaymentMethodFragment()).commit()
-        }
-
-        binding.notificationButton.setOnClickListener {
-
-            parentFragmentManager.beginTransaction().replace(R.id.frame_container, NotificationFragment()).commit()
+            parentFragmentManager.beginTransaction().replace(R.id.frame_container, ProfileFragment()).commit()
 
         }
 
     }
-
 }
